1d604e0724e32643e6dd31d779ebe0b2752855cc
aya hussein <aya.hussein3107@gmail.com>
2021-04-25 19:58:53 +0200

    lab1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    constructor(){
+        super();
+        this.state={
+            counter:0,
+            data:{
+                id:"1",
+                jobTitleName:"Developer",
+                firstName:"Ahmed",
+                lastName:"Ali",
+                fullName:"Ahmed Ali",
+                employeeCode:"E1",
+                region:"CA",
+                phoneNumber:"408-1234567",
+                emailAddress:"ahmed.ali@gmail.com"
+            }
+        }
+    }
+
+    changeData=(d)=>{
+        this.setState({counter:d})
+    }
+    render() {
+        let style={
+            color:"Cyan"
+        }
+        return (
+            <div className="App" >
+                <div className="App-header">
+                    <Counter counter={this.state.counter} changeFn={this.changeData} > </Counter>
+                    <h4 style={style}>{this.state.counter}</h4>
+                    <p> {JSON.stringify(this.state.data)}</p>  
+
+                </div>
+            </div>
+        );
+    }
 }
 
-export default App;
+
+
+class Counter extends Component {
+    constructor(props){
+        super();
+        this.state={
+            counter:props.counter
+    }
+}
+
+    changeCounter = (op)=>{
+        if(op=="+"){
+            this.props.changeFn(this.state.counter+1);
+            this.setState({counter:this.state.counter+1});
+            
+        }
+        else if(op=="-"){
+            this.props.changeFn(this.state.counter-1);
+            this.setState({counter:this.state.counter-1});
+            
+        }
+
+
+        
+
+    }
+
+    render() {
+        return ( 
+        <div>                 
+            <h1>{this.state.counter}</h1>    
+            <button onClick={()=>{this.changeCounter("+")}}>Increase</button>
+            <button onClick={()=>{this.changeCounter("-")}}>Decrease</button>
+        </div>);
+        }
+    }
+    export default App;
